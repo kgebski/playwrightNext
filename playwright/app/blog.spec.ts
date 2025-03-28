1a18b6f7557c1648a5d77421b47017cd43991289
Kyle Gebski <kgebski@hagerty.com>
2025-03-28 08:46:16 -0400

    commit before Colin Adam meeting

--- playwright/app/blog.spec.ts
+++ playwright/app/blog.spec.ts
@@ -1,2 +1,20 @@
-This will test my blog page
-Is this where I put my selectors? 
+import { test, expect } from '@playwright/test';
+
+test('BlogCheck', async ({ page }) => {
+    await page.goto('http://localhost:3000/blog');
+    await expect(page.getByText('src/app/blog/page.tsx')).toBeVisible();
+    // Click the get started link.
+    await expect(page.getByRole('link', { name: 'Read our docs' })).toBeVisible(); // This navigates away to Next docs
+    
+    const expectInput = page.getByRole('link', { name: 'Read our docs' })
+    await expect(expectInput).toBeVisible();
+  
+    //await expect(page.getByRole('heading', { name: 'Introduction' })).toBeVisible();
+    //await expect(page).toHaveURL("https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app");
+    //await page.getByRole('button', { name: 'Search documentation...' }).click();
+    //await page.keyboard.type('styling'); //Not seeing the ui pop the serach modal in the UI test
+  
+  
+    //await page.getByRole('textbox').fill('focus');
+  })
+    // Y NO WORK? await locator.focus();
