e7a303805aca2880865ba063d2891614f320bce2
Kyle Gebski <kgebski@hagerty.com>
2025-05-09 14:00:34 -0400

    commit before reverting full size issues

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -10,10 +10,10 @@ export const metadata: Metadata = {
 
 export default function Examples() {
   return (
-    <div className="globalspage">
-      <main className="globalsmain">
+    <div className="page">
+      <main className="main">
         <Image
-          className="globalslogo"
+          className="logo"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
@@ -28,50 +28,6 @@ export default function Examples() {
           <li><code>BECAUSE CODE WAS BREAKING THIS</code></li>
         </ul>
       </main>
-      <footer className="globalsfooter">
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,58 +4,56 @@ import "./styles/globals.scss";
 import "./styles/styles.scss";
 
 export const metadata: Metadata = {
-  title: "Homepage",
-  description: "It's a homepage bro",
+title: "Homepage",
+description: "It's a homepage bro",
 };
 
 export default function Home() {
-  return (
-    <div className="page">
-      <main className="main">
-      <div className="contain">
-        <Image
-          className="logo"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1>Homepage</h1>
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+return (
+  <div className="page">
+    <main className="main">
+      <Image
+        className="logo"
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+      <h1>Homepage</h1>
+      <ol>
+        <li>
+          Get started by editing <code>src/app/page.tsx</code>.
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
 
-        <div className="ctas">
-          <a
-            className="primary"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="logo"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            className="secondary"
-          >
-            Read our docs
-          </a>
-        </div>
-        </div>
-      </main>
-      
-    </div>
-  );
+      <div className="ctas">
+        <a
+          className="primary"
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="logo"
+            src="/vercel.svg"
+            alt="Vercel logomark"
+            width={20}
+            height={20}
+          />
+          Deploy now
+        </a>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          className="secondary"
+        >
+          Read our docs
+        </a>
+      </div>
+    </main>
+    
+  </div>
+);
 }
